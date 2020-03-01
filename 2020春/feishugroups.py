5efd06ec612cf15c0200998a477506d3dda501dd
huzheng <huzheng.red@gmail.com>
2020-03-01 15:52:23 +0800

    协调资源后,X扩容到8+，更新以seed抽签出小组号为飞书项目组

--- 2020春/feishugroups.py
+++ 2020春/feishugroups.py
@@ -1,7 +1,6 @@
-seed='飞书'.encode('utf-8')
+seed='飞书小程序设计'.encode('utf-8')
 ls=list(seed)
 group=[]
-print(len(ls)//2)
 for i in range(len(ls)//2): 
     group.append((ls[i]+ls[i+3]) % 27 +1)
 group=list(set(group))
